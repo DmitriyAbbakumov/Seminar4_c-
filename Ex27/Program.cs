ccaa3a75e40f46360e1a780f117f6adc94d06f8e
Dmitriy <simpa-baki@mail.ru>
2022-08-19 00:47:17 +0300

    initial commit

---
+++ Ex27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+void sum(int a)
+{
+    int result = 0;
+    for (int temp = 0; a > 0; result += temp)
+    {
+        temp = a % 10;
+        a = a / 10;
+    }
+    Console.WriteLine(result);
+}
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+sum(n);
---
+++ Ex29/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 29: Напишите программу, которая задаёт
+массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+void Fill(int[] arr) 
+{
+    int i = 0;
+    Console.Write("[");
+    for (int length = arr.Length; i < length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+        Console.Write($" {arr[i]}");
+    }
+    Console.Write(" ]");
+}
+
+int[] massiv = new int[8];
+Fill(massiv);
